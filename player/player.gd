0b375ae92dcd5db419f0e45f2f499b04c6e82696
babichgh <ivan20.babich@gmail.com>
2026-01-27 22:56:37 +0300

    Add TileMap and animations for Player

--- player/player.gd
+++ player/player.gd
@@ -13,13 +13,31 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		 
+		
+		if direction == 1:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
+		
+		
+		if velocity.y == 0:
+			$AnimatedSprite2D.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if velocity.y == 0:
+			$AnimatedSprite2D.play("Idle")
+	
+	
+	if velocity.y > 0:
+		$AnimatedSprite2D.play("Fall")
+	
+	
 	move_and_slide()
